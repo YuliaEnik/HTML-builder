7850d9a7d5e7d21df6295372edf6dbb249800d4c
Yuliya Aleinik <aleinikya@mail.ru>
2021-11-09 00:09:33 +0300

    feat: done  4-5 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const filesFolder = path.join(__dirname, "files");
+const filesFolderCopy = path.join(__dirname, "files-copy");
 
+fs.readdir(filesFolder, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  fs.mkdir(filesFolderCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  
+  for ( let file of files) {
+    fs.copyFile(`${filesFolder}/${file.name}`,`${filesFolderCopy}/${file.name}`, (err) => {
+    if(err) throw err
+   })
+  };
+});
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs   = require('fs');
+const path = require('path');
+const bundleCss = path.join(__dirname, 'project-dist/bundle.css');
+const cssFiles = path.join(__dirname, 'styles');
+
+const buildCSS = () => {
+fs.writeFile(bundleCss, "", (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(cssFiles, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      if (path.extname(`${file.name}`) === ".css") {
+        fs.readFile(`${cssFiles}/${file.name}`, "utf-8", (err, data) => {
+          if (err) throw err;
+          else {
+            fs.appendFile(bundleCss, data, (err) => {
+              if (err) throw err;
+            });
+        }
+    });
+}
+};
+}); 
+}
+
+buildCSS()
